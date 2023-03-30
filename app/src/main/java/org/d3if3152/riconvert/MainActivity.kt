119ecc46f5df771414596619aff46a58528bc389
Aria Muhammad Luthfi <ariamuhammad77@gmail.com>
2023-03-30 16:29:32 +0700

    Masukkan nominal, Konverter spinner, Hasil

--- app/src/main/java/org/d3if3152/riconvert/MainActivity.kt
+++ app/src/main/java/org/d3if3152/riconvert/MainActivity.kt
@@ -1,11 +1,51 @@
 package org.d3if3152.riconvert
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import org.d3if3152.riconvert.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener { hitungKonversi() }
+
+
+    }
+
+
+
+    @SuppressLint("StringFormatInvalid")
+    private fun hitungKonversi(){
+        val rupiah = binding.nominal.text.toString()
+
+        if (TextUtils.isEmpty(rupiah)) {
+            Toast.makeText(this, R.string.nominal_invalid, Toast.LENGTH_LONG).show()
+            return
+        }
+        val selectedValue = binding.uang.selectedItem.toString()
+        if(selectedValue == "Dollar"){
+            val dollarRp = 15059
+            val hasil = rupiah.toFloat() / dollarRp
+            binding.hasilConvert.text = NumberFormat.getCurrencyInstance(Locale.US).format(hasil)
+            binding.kurs.text = getString(R.string.kurs, dollarRp)
+        }else if(selectedValue == "Euro"){
+            val euroRp = 16371
+            val hasil = rupiah.toFloat() / euroRp
+            binding.hasilConvert.text = NumberFormat.getCurrencyInstance(Locale.GERMANY).format(hasil);
+            binding.kurs.text = getString(R.string.kurs, euroRp)
+        }
+
+
+
     }
 }
