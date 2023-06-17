5277fe05b733dbef958a96a9e97162ac0dbde9b5
Aria Muhammad Luthfi <ariamuhammad77@gmail.com>
2023-06-17 22:01:47 +0700

    Meminta izin notifikasi

--- app/src/main/java/org/d3if3152/riconvert/ui/matauang/MataUangFragment.kt
+++ app/src/main/java/org/d3if3152/riconvert/ui/matauang/MataUangFragment.kt
@@ -1,13 +1,19 @@
 package org.d3if3152.riconvert.ui.matauang
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
+import org.d3if3152.riconvert.MainActivity
 import org.d3if3152.riconvert.databinding.FragmentUangBinding
 import org.d3if3152.riconvert.network.ApiStatus
 
@@ -56,11 +62,30 @@ class MataUangFragment : Fragment() {
             }
             ApiStatus.SUCCES -> {
                 binding.progressBar.visibility = View.GONE
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    requestNotificationPermission()
+                }
             }
             ApiStatus.FAILED -> {
                 binding.progressBar.visibility = View.GONE
                 binding.networkError.visibility = View.VISIBLE
             }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun requestNotificationPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                MainActivity.PERMISSION_REQUEST_CODE
+            )
         }
     }
 }
