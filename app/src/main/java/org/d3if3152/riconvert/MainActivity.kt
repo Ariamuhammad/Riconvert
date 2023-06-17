24093946753fbad86b5a62f2873448fef0963be2
Aria Muhammad Luthfi <ariamuhammad77@gmail.com>
2023-06-17 21:58:11 +0700

    Membuat notification channel

--- app/src/main/java/org/d3if3152/riconvert/MainActivity.kt
+++ app/src/main/java/org/d3if3152/riconvert/MainActivity.kt
@@ -1,5 +1,9 @@
 package org.d3if3152.riconvert
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
@@ -9,6 +13,10 @@ import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        const val CHANNEL_ID = "updater"
+    }
+
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +25,16 @@ class MainActivity : AppCompatActivity() {
 
         navController = findNavController(R.id.myNavHostFragment)
         NavigationUI.setupActionBarWithNavController(this, navController)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance)
+            channel.description = getString(R.string.channel_desc)
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE)
+                    as NotificationManager?
+            manager?.createNotificationChannel(channel)
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
